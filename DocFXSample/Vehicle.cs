fb71273aa38578ed5bf2932809a47007179a4626
Calvin Wilkinson <kinsondigital@gmail.com>
2022-05-12 09:16:09 +0100

    Add code docs

--- DocFXSample/Vehicle.cs
+++ DocFXSample/Vehicle.cs
@@ -2,13 +2,28 @@
 
 namespace DocFXSample;
 
+/// <summary>
+/// Represents a vehicle that can be driven.
+/// </summary>
 public class Vehicle
 {
+    /// <summary>
+    /// Gets or sets the color of the <see cref="Vehicle"/>.
+    /// </summary>
     public Color Color { get; set; }
 
+    /// <summary>
+    /// Gets or sets the year of the <see cref="Vehicle"/>.
+    /// </summary>
     public int Year { get; set; }
 
+    /// <summary>
+    /// Gets or sets the make of the <see cref="Vehicle"/>.
+    /// </summary>
     public string Make { get; set; }
 
+    /// <summary>
+    /// Gets or sets the model of the <see cref="Vehicle"/>.
+    /// </summary>
     public string Model { get; set; }
 }

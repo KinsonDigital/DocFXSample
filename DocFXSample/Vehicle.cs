4007927dfd8c732c9305698d44909166702e2645
Calvin Wilkinson <kinsondigital@gmail.com>
2022-05-12 09:15:06 +0100

    Add class to the project

---
+++ DocFXSample/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace DocFXSample;
+
+public class Vehicle
+{
+    public Color Color { get; set; }
+
+    public int Year { get; set; }
+
+    public string Make { get; set; }
+
+    public string Model { get; set; }
+}
